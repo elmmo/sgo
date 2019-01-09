f49fd1f8a52e6d50409a98b5cf7318bdcbd3f7c4
ELIZABETH MIN <elmm682@gmail.com>
2019-01-09 11:19:10 -0800

    added timepicker styles

--- options/options.js
+++ options/options.js
@@ -1,7 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(event)
-			{
-			    timepicker.load({
-			        interval: 15,
-			        defaultHour: 8
-			    });
-			});
+var options = {
+	now: "8:00", 
+	title: 'Office Hours', //The Wickedpicker's title,
+	showSeconds: false, //Whether or not to show seconds
+	timeSeparator: ' : ', // The string to put in between hours and minutes (and seconds)
+	secondsInterval: 1, //Change interval for seconds, defaults to 1,
+	minutesInterval: 10, //Change interval for minutes, defaults to 1
+	clearable: false, //Make the picker's input clearable (has clickable "x")
+};
+
+var timepickers = $('.timepicker').wickedpicker(options); 
+console.log(timepickers.wickedpicker('time', 1));
--- timepicker/README.md
+++
@@ -1,5 +0,0 @@
-# Timepicker
-Let users pick a time from a dropdown menu.
-Intervals can be defined, default is 15 minutes.
-
-User first pick an hour then a minute, output will be hh:mm
