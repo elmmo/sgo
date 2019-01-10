5917e3e73aa8a2f09adbd303d469597d2361e337
ELIZABETH MIN <elmm682@gmail.com>
2019-01-09 21:40:28 -0800

    added Chrome storage integration

--- options/options.js
+++ options/options.js
@@ -1,3 +1,9 @@
+// showing the profiles already stored in options
+$(function() {
+	showProfiles(); 
+}); 
+
+// initializing timepicker 
 var options = {
 	now: "8:00", 
 	title: 'Office Hours', //The Wickedpicker's title,
@@ -8,5 +14,50 @@ var options = {
 	clearable: false, //Make the picker's input clearable (has clickable "x")
 };
 
+// global variables 
+var keys = Array(20); 
+console.log(keys); 
 var timepickers = $('.timepicker').wickedpicker(options); 
-console.log(timepickers.wickedpicker('time', 1));
+
+// store information upon submit 
+$("#addProf").submit(function(e) {
+	let entry = $(this).serializeArray()
+	let title = entry[0].value; 
+	let timeData = [entry[1].value, entry[2].value]; 
+	chrome.storage.sync.set({[title]: timeData}, function() {
+		console.log("Office hours for " + title + " stored.");
+		addProfile(title, timeData[0], timeData[1]); 
+	}); 
+	e.preventDefault();
+});
+
+// shows profiles already in storage
+var showProfiles = function() {
+	chrome.storage.sync.get(null, function(profiles) {
+		let i = 0; 
+		for (p in profiles) {
+			addProfile(p, profiles[p][0], profiles[p][1]); 
+			keys[i] = p; 
+			i++; 
+		}
+	}); 
+}
+
+var addProfile = function(nameInput, startTime, endTime) {
+	if (false /*keys.includes(nameInput)*/) {
+	} else {
+		let profile = document.createElement("DIV");
+		profile.className = "profile"; 
+		profile.setAttribute("id", nameInput.toLowerCase()); 
+
+		let name = document.createElement("P");
+		profile.appendChild(name);
+		let time = document.createElement("P");
+		profile.appendChild(time);
+
+		name.appendChild(document.createTextNode(nameInput)); 
+		time.appendChild(document.createTextNode(startTime + " to " + endTime)); 
+
+		$("#profiles").append(profile); 
+	}
+}
